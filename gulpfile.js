da1764a7341e3047d328034d4c474bc30e14891d
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-08-02 23:43:08 +0300

    forms

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 // const dist = "./dist/";
-const dist = "/ospanel/domains/sourece2";
+const dist = "/OSPanel/domains/source";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
